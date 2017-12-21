b2cef332913f5fa0b8ba4c48c5e6fb578e3f10ed
keweixinfr <keweixinfr@gmail.com>
2017-12-21 10:01:16 +0100

    [UPDATE]identify class url

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var fs = require('fs');
 var multer = require('multer')
 app.use(express.static('client'));
 var process = require("process");
+var im = require("imagemagick"); 
 
-const port			= process.env.PORT || 4242;
+const port          = process.env.PORT || 4242;
 
 // Multer storage options
 var storage = multer.diskStorage({
@@ -18,11 +19,11 @@ var storage = multer.diskStorage({
 });
 
 var watson = require('watson-developer-cloud');
-	var visual_recognition = watson.visual_recognition({
-	  api_key: '45ac944541bb307b8591284c90aa39aa18d7dfa5',
-	  version: 'v3',
-	  version_date: '2016-05-20'
-	});
+    var visual_recognition = watson.visual_recognition({
+      api_key: '45ac944541bb307b8591284c90aa39aa18d7dfa5',
+      version: 'v3',
+      version_date: '2016-05-20'
+    });
 
 
 var upload = multer({ storage: storage });
@@ -32,35 +33,65 @@ app.get('/', function (req, res) {
 });
 
 app.post('/upload', upload.single('image'), function(req, res, next){
+    // try{
+    //     im.crop({
+    //           srcPath: 'test.jpg',
+    //           dstPath: 'testout.jpg',
+    //           width: 800,
+    //           height: 600,
+    //           quality: 1,
+    //           gravity: "North"
+    //     }, function(err, stdout, stderr){
+    //       // foo
+    //     });
+    // }catch(err){
+    //     consolo.log(err)
+    // }
+
+    // -------------------------------------------------------------
+    // var params = {
+    //   images_file: fs.createReadStream('./client/uploads/image.jpg')
+    // };
+
+    // visual_recognition.detectFaces(params,
+    //   function(err, response) {
+    //     if (err)
+    //       console.log(err);
+    //     else
+    //       console.log(JSON.stringify(response, null, 2));
+
+    //   });
 
 
-	var params = {
-  		images_file: fs.createReadStream('./client/uploads/image.jpg'),
-  		parameters: {
-  			"classifier_ids":["FacerReader_878661806"]
-  		}
-	};
-	visual_recognition.classify(params, function(err, response) {
-		if (err){
-		    console.log(error);
-		}
-		else{
-		    console.log(JSON.stringify(response, null, 2));
-		    var result = response["images"][0]["classifiers"]
-		    if (result.length>0)
-		    {
-				var resultname = result[0]["classes"][0]["class"]
-			}
-			else{
-				var resultname = "Personne"
-			}
-			res.send(resultname);
-		}
-	});
+
+// -------------------------------------------------------------------------
+    var params = {
+         images_file: fs.createReadStream('./client/uploads/image.jpg'),
+         parameters: {
+             "classifier_ids":["FaceReader_V2_1730969211"]
+         }
+    };
+    visual_recognition.classify(params, function(err, response) {
+     if (err){
+         console.log(error);
+     }
+     else{
+         console.log(JSON.stringify(response, null, 2));
+         var result = response["images"][0]["classifiers"]
+         if (result.length>0)
+         {
+             var resultname = result[0]["classes"][0]["class"]
+         }
+         else{
+             var resultname = "Personne"
+         }
+         res.send(resultname);
+     }
+    });
 
     
 });
 
 app.listen(port, function () {
-  console.log('App listening on port 3000!');
+  console.log('App listening on port 4242!');
 });
--- request.py
+++ request.py
@@ -4,10 +4,11 @@ url="https://facereader.herokuapp.com/upload/"
 files = {'image': open('test.jpg', 'rb')}
 
 r = requests.post(url,files=files)
-print(r.text)
+print r.text
 
 with open( 'test.jpg','rb') as imgdata:
     files = {'image': imgdata}
     response = requests.post(url,files=files)
     result=response.text
-    print(result)
+    print response
+    print result
