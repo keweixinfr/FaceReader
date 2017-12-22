cb26771ee13a5e4f3365363f05efcf0f5d67a4e2
keweixinfr <keweixinfr@gmail.com>
2017-12-22 15:20:53 +0100

    [TEST]deploy on server

--- app.js
+++ app.js
@@ -37,33 +37,91 @@ app.get('/', function (req, res) {
 app.post('/upload', upload.single('image'), function(req, res, next){
 
 
-    // --------------------Imagemagick Version--------------------------------------------
-    im.crop({
-      srcPath: __dirname + '/client/uploads/image.jpg',
-      dstPath:  __dirname + '/client/uploads/imageresized.jpg',
-      width: 800,
-      height: 600,
-      quality: 1,
-      gravity: "North"
-    }, function(err, stdout, stderr){
-        if(err){
-            console.log(err)
-        }else{
-            console.log('stdout:', stdout);
-        }
-    });
-    // --------------------GM Version--------------------------------------------
-    // gm('./client/uploads/image.jpg')
-    //     .resize(240, 240, '!')
-    //     .write('/client/uploads/imageresized.jpg', function (err) {
-    //         if (err){
-    //             console.log(err);
-    //             res.send(err)
+    // --------------------facedetect function--------------------------------------------
+    var params_face = {
+        images_file: fs.createReadStream('./client/uploads/image.jpg'),
+    };
+
+    visual_recognition.detectFaces(params_face,
+        function(error_detect, response) {
+            if (error_detect){
+                console.log(error_detect);
+                res.send("Personne")
+            }
+            else{
+                //TODO: if judgement
+                faceArray= response["images"][0]["faces"]
+                if(faceArray.length){
+                    //if several persons present, chose the first one in the face array
+                    faceLocation =faceArray[0]["face_location"]
+                    
+                    var srcPath = __dirname + '/client/uploads/image.jpg'
+                    var dstPath = __dirname + '/client/uploads/imageresized.jpg'
+                    var x=faceLocation["left"] // topright location : left 2 right
+                    var y=faceLocation["top"]  // topright location : top 2 bottom
+                    var height=faceLocation["height"] // crop size height
+                    var width =faceLocation["width"] // crop size width
+                    var args = [
+                        srcPath,
+                        "-crop",
+                        width+"x"+height+"+"+x+"+"+y,
+                        dstPath
+                    ];
+                    im.convert(args, function(error_crop) {
+                        if(error_crop){
+                            console.log(error_crop)
+                            res.send("Personne")
+                        }else{
+                            var params = {
+                                 images_file: fs.createReadStream('./client/uploads/imageresized.jpg'),
+                                 parameters: {
+                                     "classifier_ids":["FaceReader_V2_1730969211"]
+                                 }
+                            };
+                            visual_recognition.classify(params, function(error_classify, IBMresponse) {
+                             if (error_classify){
+                                 console.log(error_classify);
+                                 res.send("Personne");
+                             }
+                             else{
+                                 console.log(JSON.stringify(IBMresponse, null, 2));
+                                 var result = IBMresponse["images"][0]["classifiers"];
+                                 if (result.length>0)
+                                 {
+                                     var resultname = result[0]["classes"][0]["class"]
+                                 }
+                                 else{
+                                     var resultname = "Personne"
+                                 }
+                                 res.send(resultname);
+                            }
+                         });
+                        }
+                    });
+                }else{
+                    res.send("Personne")
+                }
+            }
+        });
+
+//-----------------------Crop function--------------------------------------------
+    // var srcPath = __dirname + '/client/uploads/image.jpg'
+    //     var dstPath = __dirname + '/client/uploads/imageresized.jpg'
+    //     var x=50 // left 2 right
+    //     var y=0  // top 2 botten
+    //     var args = [
+    //         srcPath,
+    //         "-crop",
+    //         "120x80+"+x+"+"+y,
+    //         dstPath
+    //     ];
+    //     im.convert(args, function(err) {
+    //         if(err){
+    //             console.log(err)
     //         }else{
-    //             res.sendFile( __dirname + "/client/uploads/imageresized.jpg");
+    //             res.sendFile(dstPath);
     //         }
     //     });
-
 
 // ----------------------Origin Version-------------------------------------------
     // var params = {
